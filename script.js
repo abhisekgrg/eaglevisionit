b57619c93a50f127c7700db0921b72a6f390761f
Rana-tech <abhisek.gurung590@gmail.com>
2025-05-21 02:08:14 +0545

    updated sections

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const mobileMenu = document.getElementById('mobile-menu');
+    const navMenu = document.querySelector('.nav-menu');
+    
+    mobileMenu.addEventListener('click', function() {
+        mobileMenu.classList.toggle('active');
+        navMenu.classList.toggle('active');
+    });
+
+    // Close the menu when a nav link is clicked
+    const navLinks = document.querySelectorAll('.nav-list li a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            mobileMenu.classList.remove('active');
+            navMenu.classList.remove('active');
+        });
+    });
+});
